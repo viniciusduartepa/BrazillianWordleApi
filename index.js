65ba5aba7082694a4b99e7f072696900a1852842
Vinicius Alves <viniciusdpalves@gmail.com>
2023-11-25 20:45:07 -0300

    missing letter string add

--- index.js
+++ index.js
@@ -36,22 +36,13 @@ app.get("/valid/:word", async (req, res) => {
     );
     if (validWord.rows.length == 0)
       res.status(404).json({ error: "Word not found in dictionary" });
-    res.status(200).json({ word: validWord.rows[0].word });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: "Server error" });
-  }
-});
 
-app.get("/check/:word", async (req, res) => {
-  try {
-    const word = req.params.word;
-    console.log("Rota: check/" + word);
     const dailyWordResult = await client.query("SELECT * FROM daily_word");
     let dailyWord = null;
     if (dailyWordResult.rows.length > 0) {
       dailyWord = dailyWordResult.rows[0].word;
     }
+    let missingLetters = dailyWord;
 
     let results = {};
     for (let i = 0; i < word.length; i++) {
@@ -59,28 +50,29 @@ app.get("/check/:word", async (req, res) => {
       const charDailyWord = dailyWord[i];
       if (charWord === charDailyWord) {
         results[i] = "correct";
-        dailyWord = dailyWord.substring(0, i) + dailyWord.substring(i + 1);
-      } else if(!dailyWord.includes(charWord)){
+        missingLetters = missingLetters.substring(0, i) + missingLetters.substring(i + 1);
+      } else if (!dailyWord.includes(charWord)) {
         results[i] = "wrong";
-      }else results[i] = null;
+      } else results[i] = null;
     }
     for (let i = 0; i < word.length; i++) {
       const charWord = word[i];
       if (results[i] === null) {
-        if(dailyWord.includes(charWord)){
+        if (dailyWord.includes(charWord)) {
           results[i] = "misplaced";
-          charIndex = dailyWord.indexOf(charWord);
-          dailyWord = dailyWord.substring(0, charIndex) + dailyWord.substring(charIndex + 1);
-        }else results[i] = "wrong";
+          charIndex = missingLetters.indexOf(charWord);
+          missingLetters =
+            missingLetters.substring(0, charIndex) +
+            missingLetters.substring(charIndex + 1);
+        } else results[i] = "wrong";
       }
     }
-
-    res.status(200).json({results });
+    res.status(200).json({ results });
   } catch (error) {
     console.log(error);
+    res.status(500).json({ error: "Server error" });
   }
 });
-
 app.listen(config.port, () =>
   console.log("Servidor funcionando na porta " + config.port)
 );
