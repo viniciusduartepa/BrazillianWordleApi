eec83b1ef8f66e3cbfd1fa5dd7d730b8c39c3f89
Vinicius Alves <viniciusdpalves@gmail.com>
2023-11-26 23:38:50 -0300

    / route added

--- routes/word.js
+++ routes/word.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router = express.Router();
 const WordController = require("../controllers/WordController");
 
+
+router.get("", (req, res) => {
+  res.status(200).send("Server is running");
+});
 router.get("/valid/:word", WordController.validate);
 router.put("/update-daily-word", WordController.resetDailyWord);
 
