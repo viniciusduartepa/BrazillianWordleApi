1c6f6ec620b6d2d39d619bd202ee154138ef1ad2
Vinicius Alves <viniciusdpalves@gmail.com>
2023-12-02 18:39:18 -0300

    feat: add cors

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const wordRouter = require("./routes/word");
 const config = require("./config");
+var cors = require('cors');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
+
 app.use("/", wordRouter);
+
 
 app.listen(config.port, () => {
  console.log(`Server running on port ${config.port}`);
